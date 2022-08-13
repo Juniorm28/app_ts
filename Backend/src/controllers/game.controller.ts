7384570cdfcff113d0bdc99eb1a2d68edf793490
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-08-13 13:23:00 -0500

    Paso 13

--- Backend/src/controllers/game.controller.ts
+++ Backend/src/controllers/game.controller.ts
@@ -26,6 +26,31 @@ async create (req: Request, res: Response){
     }
 }
 
+async update(req: Request, res: Response) {
+    try {
+
+        const games = await Game.findByPk(req.params.id);
+
+        //validar si existe
+        if (!games) {
+            return res.status(404).send('El juego que intenta altualizar no existe');
+        }
+        //actualizar usuario
+        await Game.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        //respuesta
+        res.status(200).send({message:'El juego ha sido modificado'});
+
+        //capturamos errores
+    } catch (e) {
+        res.status(500).send('error interno, intente mas tarde');
+    }
+};
+
 
 async delete(req: Request, res: Response){
     try {
--- Backend/src/routes/game.ts
+++ Backend/src/routes/game.ts
@@ -9,6 +9,7 @@ export class GameRoutes {
     public ruta(app: Application) {
 
         app.route("/games").get(this.gameController.index);
+        app.route("/games/:id").put(this.gameController.update);
         app.route("/games").post(this.gameController.create);
         app.route("/games/:id").delete(this.gameController.delete);
 
