aed13a020f833745a19cf526b820b07daa2d16d4
Juniorm28 <martinezlopezjunior28@gmail.com>
2022-08-14 10:07:36 -0500

    Paso 27

--- Frontend/src/app/models/game-i.ts
+++ Frontend/src/app/models/game-i.ts
@@ -1,2 +1,8 @@
 export interface GameI {
+    id?: number,
+    title?: string,
+    description?: string,
+    image?: string,
+    createdAt?: string,
+    updatedAt?: string
 }
--- Frontend/src/app/services/games.service.ts
+++ Frontend/src/app/services/games.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GameI } from '../models/game-i';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
-  constructor() { }
+  API_URI = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) { }
+
+  getClients() {
+    return this.http.get(`${this.API_URI}/games`);
+  }
 }
